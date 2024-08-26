dab08a31f9cebd4e2175cefdd90991b6280d00a6
Daniel Peralta <dp25443@gmail.com>
2024-08-26 18:48:42 -0300

    Modificación de parametros en metodo convertFloatToIEEE

--- src/main/java/ar/com/dperalta/conversor/controller/Ieeep754Controller.java
+++ src/main/java/ar/com/dperalta/conversor/controller/Ieeep754Controller.java
@@ -2,6 +2,7 @@ package ar.com.dperalta.conversor.controller;
 
 import ar.com.dperalta.conversor.service.Ieeep754Service;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
@@ -13,12 +14,14 @@ public class Ieeep754Controller {
     private Ieeep754Service ieeep754Service;
 
     @PostMapping("/float")
-    public Map<String, String> floatToIEEE754(@RequestBody float value) {
-        return ieeep754Service.floatToIEEE754(value);
+    public ResponseEntity<Map<String, String>> convertFloatToIEEE754(@RequestBody Map<String, Float> request) {
+        Float value = request.get("float");
+        Map<String, String> result = ieeep754Service.floatToIEEE754(value);
+        return ResponseEntity.ok(result);
     }
 
     @PostMapping("/ieee")
-    public float ieee754ToFloat(String sign, String exponent, String mantissa) {
+    public float ieee754ToFloat(@RequestBody String sign, @RequestBody String exponent, @RequestBody String mantissa) {
         return ieeep754Service.ieee754ToFloat(sign, exponent, mantissa);
     }
 }
