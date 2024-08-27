25234bf06309c829db6ea983935e39fb428d3e19
Daniel Peralta <dp25443@gmail.com>
2024-08-26 21:43:35 -0300

    se modificaron los endpoint para acceder por el front de manera mas organizada

--- src/main/java/ar/com/dperalta/conversor/config/CorsConfig.java
+++ src/main/java/ar/com/dperalta/conversor/config/CorsConfig.java
@@ -1,9 +1,12 @@
 package ar.com.dperalta.conversor.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * Configuración para evitar error de CORS al estar en producción.
+ */
 
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
@@ -11,7 +14,10 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://conversor.dperalta.com.ar/")
+                .allowedOrigins("https://dperalta.com.ar/",
+                                "https://digitales1.dperalta.com.ar/",
+                                "https://digitales1.dperalta.com.ar/conversor",
+                                "https://digitales1.dperalta.com.ar/ieee754")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
